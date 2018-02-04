7ed63b09802b9e5c02504de30cbfade9c5e9e047
Eike J. Raielo <eikejraielo@gmail.com>
2018-02-04 09:53:38 +0000

    feat(app.js): design

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+class Greeting extends React.Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
 
 export default class App extends React.Component {
   render() {
+    let pic = {
+      uri: 'http://www.freewebs.com/opengatesfarmbedandbreakfast/alarmclock6am.png'
+    };
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={{fontSize: 18}}>Welcome to AlarmApp!</Text>
+        <Text>This wakes you up in a new way.</Text>
+        <Image source={pic} style={{width: 150, height: 180}}/>
+        <Greeting name='Mari' />
+        <Greeting name='Kaisa' />
+        <Greeting name='Andrew' />
       </View>
     );
   }
